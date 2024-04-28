8ec86a85a08cda971241fa184b760f156f76823a
Cristiano Stroher <120978880+CristianoStroher@users.noreply.github.com>
2024-04-28 20:23:41 -0300

    conserto do erro

--- lib/app/repositories/user/user_repository_impl.dart
+++ lib/app/repositories/user/user_repository_impl.dart
@@ -1,11 +1,12 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:todo_list_provider/app/exception/auth_exception.dart';
 import './user_repository.dart';
 
 class UserRepositoryImpl implements UserRepository {
 
-  FirebaseAuth _firebaseAuth; //cria uma instânica do tipo FirebaseAuth
+  final FirebaseAuth _firebaseAuth; //cria uma instânica do tipo FirebaseAuth
   
   //Constructor
   UserRepositoryImpl({
@@ -25,15 +26,31 @@ class UserRepositoryImpl implements UserRepository {
     print(e);
     print(s);
     if(e.code == 'email-already-in-use') {
-      final loginTypes = await _firebaseAuth.fetchSignInMethodsForEmail(email);
-      if(loginTypes.contains('password')){
+      //!como foi depreciado o método fetchSignInMethodsForEmail, vamos fazer a verificação de outra forma.
+      //! verificar se o email já está cadastrado no firebase
+      final loginTypes = await FirebaseFirestore
+        .instance.collection('users')
+        .where('email', isEqualTo: email)
+        .get();
+      //! se o loginTypes.docs não estiver vazio, então o e-mail já está cadastrado
+      if(loginTypes.docs.isNotEmpty){
         throw AuthException(
           message: 'E-mail já cadastrado');
+      //! se não, então o e-mail já está cadastrado com o Google
       } else {
         throw AuthException(
           message: 'Você já se cadastrou com o Google, por favor utilize esse método para logar');
-      }
+      } 
+      // final loginTypes = await _firebaseAuth.fetchSignInMethodsForEmail(email);
+      // if(loginTypes.contains('password')){
+      //   throw AuthException(
+      //     message: 'E-mail já cadastrado');
+      // } else {
+      //   throw AuthException(
+      //     message: 'Você já se cadastrou com o Google, por favor utilize esse método para logar');
+      // }
      } else {
+      //!outro tipo de erro, lançar exceção padrão
       throw AuthException(
         message: e.message ?? 'Erro ao registrar usuário');
      }
