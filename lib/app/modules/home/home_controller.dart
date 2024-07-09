ace8e855c10cd51e530f97212f5d8551168015f4
Cristiano Stroher <120978880+CristianoStroher@users.noreply.github.com>
2024-07-08 22:03:56 -0300

    taskmodule

--- lib/app/modules/home/home_controller.dart
+++ lib/app/modules/home/home_controller.dart
@@ -1,7 +1,16 @@
 import 'package:todo_list_provider/app/core/notifier/default_change_notifier.dart';
 import 'package:todo_list_provider/app/models/task_filter_enum.dart';
+import 'package:todo_list_provider/app/services/task/tasks_service.dart';
+
 
 class HomeController extends DefaultChangeNotifier {
+
+  //cria uma variável do tipo TasksService para receber o serviço de tarefas
+  final TasksService _tasksService; 
+
+  //cria um construtor que recebe um parametro do tipo TasksService
+  HomeController({ required TasksService tasksService})
+   : _tasksService = tasksService; 
 
   var filterSelected = TaskFilterEnum.today; //cria uma variável do tipo TaskFilterEnum e atribui o valor today
   
--- lib/app/modules/home/home_module.dart
+++ lib/app/modules/home/home_module.dart
@@ -1,21 +1,38 @@
-
 import 'package:path/path.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_list_provider/app/core/modules/todo_list_module.dart';
 import 'package:todo_list_provider/app/modules/home/home_controller.dart';
 import 'package:todo_list_provider/app/modules/home/home_page.dart';
+import 'package:todo_list_provider/app/repositories/tasks/tasks_repository.dart';
+import 'package:todo_list_provider/app/repositories/tasks/tasks_repository_impl.dart';
+import 'package:todo_list_provider/app/services/task/tasks_service.dart';
+import 'package:todo_list_provider/app/services/task/tasks_service_impl.dart';
 
 class HomeModule extends TodoListModule {
+  HomeModule()
+      : super(
+            bindings: [
+              //instanciando o TasksRepositoryImpl para ser usado no controller
+              // e no provider para ser usado no banco de dados
+              Provider<TasksRepository>(
+                create: (context) => TasksRepositoryImpl(
+                  sqliteConnectionFactory: context.read(),
+                ),
+              ),
 
-  HomeModule() : super (
-     bindings: [ ChangeNotifierProvider( //cria um provider para o ChangeNotifier
-        create: (context) => HomeController()), //lista de bindings que serve para injetar dependências
+              //instanciando o TasksServiceImpl para ser usado no controller
+              Provider<TasksService>(
+                create: (context) =>
+                    TasksServiceImpl(tasksRepository: context.read()),
+              ),
 
-     ], //lista de bindings que serve para injetar dependências
-    routers: {
-      '/home': (context) => const HomePage(), //rota para a Home Page
-    }
-  );
-
-  
+              ChangeNotifierProvider(
+                  //cria um provider para o ChangeNotifier
+                  create: (context) => HomeController(
+                      tasksService: context
+                          .read())), //lista de bindings que serve para injetar dependências
+            ], //lista de bindings que serve para injetar dependências
+            routers: {
+              '/home': (context) => const HomePage(), //rota para a Home Page
+            });
 }
